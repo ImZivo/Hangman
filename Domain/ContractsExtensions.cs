7c314fc67fb3303e2fb8700f7a989e650e19013c
Austin Van Bockern <austinvanbockern@users.noreply.github.com>
2020-09-13 16:27:04 -0500

    Cleaned up skeleton Other misc code cleanup

--- Domain/ContractsExtensions.cs
+++ Domain/ContractsExtensions.cs
@@ -11,10 +11,13 @@ namespace Hangman.Domain
                 throw new ArgumentNullException(nameof(@this));
             }
 
-            return new Puzzle(@this.PuzzleId, @this.Text, @this.BadGuesses, @this.Status.ToModel());
+            return new Puzzle(@this.PuzzleId, @this.Text, @this.Guesses, @this.Status.ToModel());
         }
 
+        public static int ToInt(this Contracts.PuzzleStatus @this) =>
+            (int)@this; // TODO: not sure if this works like i think
+
         public static PuzzleStatus ToModel(this Contracts.PuzzleStatus @this) =>
-            (PuzzleStatus)(int)@this; // TODO: not sure if this works like i think
+            (PuzzleStatus)@this.ToInt(); // TODO: not sure if this works like i think
     }
 }
--- Service/Controllers/PuzzleController.cs
+++ Service/Controllers/PuzzleController.cs
@@ -17,20 +17,9 @@ namespace Hangman.Service.Controllers
         }
 
         [HttpGet("{id}")]
-        public Contracts.Puzzle Get([FromRoute] string id) // this could be string, id, whatever... but it needs to be a primitive
+        public Contracts.Puzzle Get([FromRoute] int id)
         {
             // TODO: get puzzle based on ID
-
-            throw new NotImplementedException();
-        }
-
-        [HttpGet("{id}/{guess}")]
-        public Contracts.Puzzle Get(
-            [FromRoute] string id, // this could be string, id, whatever... but it needs to be a primitive
-            [FromRoute] string guess,
-            [FromBody] Contracts.Puzzle puzzle)
-        {
-            // TODO: calculate
 
             throw new NotImplementedException();
         }
