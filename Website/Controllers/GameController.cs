bd8aa36fa12f25d9cea68b85daf737ea5e3c0c08
Austin Van Bockern <austinvanbockern@users.noreply.github.com>
2020-09-16 18:27:08 -0500

    Modified UI slightly Added code to handle cases where the puzzle is solved Implemented a temp puzzle solving solution in the game controller for testing

--- Website/Controllers/GameController.cs
+++ Website/Controllers/GameController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Hangman.Domain;
 using Hangman.Website.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -7,20 +8,46 @@ namespace Hangman.Website.Controllers
 {
     public sealed class GameController : Controller
     {
+        private readonly (int Id, string Text)[] Solutions =
+        {
+            (1, "ALPHABETICAL"),
+            (2, "PUZZLE"),
+            (3, "FORGERY"),
+            (4, "FACTORY"),
+            (5, "CHALLENGE")
+        };
+
         public IActionResult Index()
         {
-            var puzzle = new Puzzle(1, "_____");
+            var rand = new Random();
+            var puzzleId = rand.Next(1, 6);
+            var puzzleText = Solutions.Single(s => s.Id == puzzleId).Text;
+            var puzzle = new Puzzle(puzzleId, String.Join(String.Empty, Enumerable.Repeat("_", puzzleText.Length)));
             var model = new GameModel(puzzle);
             return View(model);
         }
 
         public Contracts.Puzzle Guess(Contracts.PuzzleGuesses puzzle)
         {
+            var solutionText = Solutions.Single(s => s.Id == puzzle.PuzzleId).Text;
+            var solutionLetters = solutionText.Distinct();
+            var currentPuzzle = solutionText;
+
+            foreach (var letter in solutionLetters)
+            {
+                if (!puzzle.Guesses.Contains(letter))
+                {
+                    currentPuzzle = currentPuzzle.Replace(letter, '_');
+                }
+            }
+
             return new Contracts.Puzzle()
             {
                 PuzzleId = puzzle.PuzzleId,
-                Text = "_PP__",
-                Status = Contracts.PuzzleStatus.None
+                Text = currentPuzzle,
+                Status = !currentPuzzle.Contains('_')
+                    ? Contracts.PuzzleStatus.Won
+                    : Contracts.PuzzleStatus.None
             };
         }
     }
