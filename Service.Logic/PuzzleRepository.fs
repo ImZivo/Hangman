d285acbf772e0356ca6cc9698cbec387743b26cf
Austin Van Bockern <austinvanbockern@users.noreply.github.com>
2020-09-24 23:02:08 -0500

    Added base implementation with TODOs for Puzzle Repository

--- Service.Logic/PuzzleRepository.fs
+++ Service.Logic/PuzzleRepository.fs
@@ -3,5 +3,16 @@ open System.Threading.Tasks
 open Hangman.Domain
 
 type IPuzzleRepository =
-    abstract member GetRandomAsync : Task<Puzzle>
+    abstract member GetRandomAsync : unit -> Task<Puzzle>
     abstract member GetAsync : puzzleId:int -> Task<Puzzle>
+
+type PuzzleRepository() =
+    interface IPuzzleRepository with
+        member _this.GetRandomAsync() =
+            async {
+                return new Puzzle(1, "TODO: finish implementation")
+            } |> Async.StartAsTask
+        member _this.GetAsync puzzleId = 
+            async {
+                return new Puzzle(puzzleId, "TODO: finish implementation")
+            } |> Async.StartAsTask
